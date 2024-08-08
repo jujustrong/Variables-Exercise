a526150a4d4bc0f688319f8535a7af3838b4316e
jujustrong <127387379+jujustrong@users.noreply.github.com>
2024-08-08 09:40:05 -0600

    Update Program.cs

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -9,7 +9,7 @@
             char foxInitial = 'K';
             bool isAwesome = true;
             double foxHeight = 15.5;
-            decimal foxLifeSpan = 200;
+            decimal foxLifeSpan = 200.6m;
             
             Console.WriteLine($"Hey my name is {foxName} but you can call me {foxInitial}! " +
                               $"I am {foxAge} years old and its {isAwesome} I am awesome! I am {foxHeight}in tall " +

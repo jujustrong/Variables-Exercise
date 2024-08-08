b27c3bc361ba5124fec7e968ba8a22342e9e3628
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-07 23:17:41 -0600

    completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string foxName = "Kit";
+            int foxAge = 7;
+            char foxInitial = 'K';
+            bool isAwesome = true;
+            double foxHeight = 15.5;
+            decimal foxLifeSpan = 200;
+            
+            Console.WriteLine($"Hey my name is {foxName} but you can call me {foxInitial}! " +
+                              $"I am {foxAge} years old and its {isAwesome} I am awesome! I am {foxHeight}in tall " +
+                              $"and i will live for {foxLifeSpan} years.");
+
         }
     }
 }
